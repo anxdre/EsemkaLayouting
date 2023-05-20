b89039d1ee78a241635ff2fec9c6e8d43987a13e
anxdre <anxdre@protonmail.com>
2023-05-20 11:50:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/esemkalayouting/BorrowingDetail.kt
@@ -0,0 +1,11 @@
+package com.example.esemkalayouting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BorrowingDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_borrowing_detail)
+    }
+}
---
+++ app/src/main/java/com/example/esemkalayouting/ThreadDetail.kt
@@ -0,0 +1,11 @@
+package com.example.esemkalayouting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThreadDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thread_detail)
+    }
+}
